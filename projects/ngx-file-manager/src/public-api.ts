4d57984d0775f43383e83291e94f3b47964b550e
Dzoibo ranites <ivandztitan@gmail.com>
2023-01-31 10:50:18 +0200

    fixing issue with consomation

--- projects/ngx-file-manager/src/public-api.ts
+++ projects/ngx-file-manager/src/public-api.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of ngx-file-manager
  */
-
-export * from './lib/ngx-file-manager.service';
-export * from './lib/ngx-file-manager.component';
 export * from './lib/ngx-file-manager.module';
+export * from './lib/ngx-file-manager.component';
+export * from './lib/ngx-file-manager.service';
